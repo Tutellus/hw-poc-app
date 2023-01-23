7cf2dc7aff450641ae7d25401524166e9f4cc507
victormer <victormerinokosina@protonmail.com>
2023-01-23 20:14:47 +0400

    feat(injected-wallet) try integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,36 @@
-import { Layout } from '@/components/layout/Layout'
-import { MainContextProvider } from '@/state/main.context'
 import '@/styles/globals.css'
+import { MainContextProvider } from '@/state/main.context'
+import { Layout } from '@/components/layout/Layout'
+import { Web3OnboardProvider, init } from '@web3-onboard/react'
+import injectedModule from '@web3-onboard/injected-wallets'
+
+const injected = injectedModule();
+
+const ethereumGoerli = {
+  id: '0x5',
+  token: 'gETH',
+  label: 'Ethereum Goerli',
+  rpcUrl: 'https://rpc.ankr.com/eth_goerli',
+}
+
+const onboard = init({
+  wallets: [injected],
+  chains: [ethereumGoerli],
+  appMetadata: {
+    name: 'SMW',
+    icon: <svg></svg>, // svg string icon
+    description: 'SMW',
+  },
+})
 
 export default function App({ Component, pageProps }) {
-  return <MainContextProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </MainContextProvider>
+  return (
+    <Web3OnboardProvider web3Onboard={onboard}>
+      <MainContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </MainContextProvider>
+    </Web3OnboardProvider>
+  );
 }
