bf4b14803810a2d3d5108f71173efcb0af38b9b1
victormer <victormerinokosina@protonmail.com>
2023-01-24 15:26:33 +0400

    feat(routes) redirect useEffect

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,24 @@ import { useEffect } from "react";
 export default function Home() {
   const { redirect } = useMainContext();
 
-  const start = async () => {
-    await redirect();
-  }
+  // const start = async () => {
+  //   await redirect();
+  // }
+
+  useEffect(() => {
+    redirect();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: '50px'
-    }}>
-      <button onClick={start}>Start</button>
-    </div>
+    null
+    // <div style={{
+    //   display: 'flex',
+    //   justifyContent: 'center',
+    //   alignItems: 'center',
+    //   marginTop: '50px'
+    // }}>
+    //   <button onClick={start}>Start</button>
+    // </div>
   );
 }
