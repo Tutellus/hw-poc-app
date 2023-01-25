22b470621cbaff39a2a461183e741d09e3e0db76
victormer <victormerinokosina@protonmail.com>
2023-01-26 01:02:49 +0400

    fix(api) execute owner tx execution hash

--- pages/api/usecases/safe/executeOwnerTransaction.js
+++ pages/api/usecases/safe/executeOwnerTransaction.js
@@ -1,11 +1,8 @@
-import { ethers } from 'ethers'
 import { config } from '../../config';
 import { getSafeData } from '../../utils/safe'
 import { getOne as getOneTx, update as updateTx, markAsExecuted } from '../../repositories/txs';
 import { getOne as getOneDID } from '../../repositories/dids';
 import { execute as executeTx } from './execute';
-
-import GnosisSafe from '../../abi/GnosisSafe.json'
 
 export async function execute({
   txId,
@@ -39,39 +36,10 @@ export async function execute({
   // Execute the transaction
   const receipt = await executeTx(tx);
 
-  // const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
-  // const serverWallet = new ethers.Wallet(serverKey, provider)
-  // const safeContract = new ethers.Contract(did.ownerMS, GnosisSafe.abi, serverWallet)
-
-  // const sortedSignatures = tx.signatures.sort((a, b) => {
-  //   const aAddress = ethers.utils.recoverAddress(tx.contractTransactionHash, a)
-  //   const bAddress = ethers.utils.recoverAddress(tx.contractTransactionHash, b)
-  //   return aAddress.localeCompare(bAddress)
-  // })
-
-  // const signaturesBytes = ethers.utils.solidityPack(
-  //   sortedSignatures.map(() => 'bytes'),
-  //   sortedSignatures
-  // );
-
-  // const executeTx = await safeContract.execTransaction(
-  //   tx.to,
-  //   tx.value,
-  //   tx.data,
-  //   tx.operation,
-  //   tx.safeTxGas,
-  //   tx.baseGas,
-  //   tx.gasPrice,
-  //   tx.gasToken || constants.AddressZero,
-  //   tx.refundReceiver || constants.AddressZero,
-  //   signaturesBytes
-  // );
-  // const receipt = await executeTx.wait()
-
   await updateTx({
     id: txId,
     fields: {
-      executionTxHash: receipt.executionTxHash,
+      executionTxHash: receipt.transactionHash,
       receipt,
     },
   })
