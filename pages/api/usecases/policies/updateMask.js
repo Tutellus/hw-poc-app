1e5b330c0e0b956b4853a45158909f86f62a5d5a
victormer <victormerinokosina@protonmail.com>
2023-01-31 18:03:35 +0400

    feat(policies) update usecases

---
+++ pages/api/usecases/policies/updateMask.js
@@ -0,0 +1,46 @@
+import { ethers } from "ethers";
+import { config } from "../../config";
+import { getOne as getProject } from "../../repositories/projects";
+import { execute as executeTransaction } from '../../infrastructure/executor';
+
+import ProxyForwardPolicies from "../../abi/ProxyForwardPoliciesMock.json"; 
+
+export default async function execute ({
+  projectId,
+  chainId,
+  address,
+  selector,
+  mask,
+}) {
+  try {
+
+    const project = await getProject({ _id: projectId });
+
+    if (!project) {
+      throw new Error('Project not found');
+    }
+    
+    const { executorKey } = project;
+    const { rpc, forwardPolicies } = config[chainId];
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+    const executor = new ethers.Wallet(executorKey, provider);
+    const forwardPoliciesInterface = new ethers.utils.Interface(ProxyForwardPolicies.abi);
+
+    const receipt = await executeTransaction({
+      to: forwardPolicies,
+      data: forwardPoliciesInterface
+        .encodeFunctionData(
+          'updateContractFunctionMask',
+          [address, selector, mask]
+        ),
+      value: 0,
+      signer: executor,
+    });
+    
+    return receipt;
+    
+  } catch (error) {
+    console.error(error)
+    throw error;
+  }
+}
