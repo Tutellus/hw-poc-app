a4468b08fd087517efa0e96f243d246df812f18a
victormer <victormerinokosina@protonmail.com>
2023-05-11 10:05:10 +0400

    fix(login) state

--- components/templates/Login.jsx
+++ components/templates/Login.jsx
@@ -1,18 +1,18 @@
 import { useWeb3Auth } from "@/state/web3auth.context";
 
 export const Login = () => {
-  const { logIn, logOut, web3Auth, loggingIn } = useWeb3Auth();
+  const { logIn, logOut, loggingIn, user } = useWeb3Auth();
   return (
     <div className="login">
       <button
         type="submit"
         onClick={logIn}
-        disabled={loggingIn}
+        disabled={loggingIn || user}
       >{loggingIn ? 'Logging in...' : 'Log in'}</button>
       <button
         type="submit"
         onClick={logOut}
-        disabled={!web3Auth}
+        disabled={!user}
       >{'Log Out'}</button>
     </div>
   )
