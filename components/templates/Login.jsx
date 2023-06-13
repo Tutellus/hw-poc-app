765b2c432a5598c0fa6214f7c9090eaa004d32fb
David Garcia <dave74@gmail.com>
2023-06-13 17:00:08 +0200

    rolback auth

--- components/templates/Login.jsx
+++ components/templates/Login.jsx
@@ -2,14 +2,14 @@ import { useWeb3Auth } from "@/state/web3auth.context"
 import { web3AuthSDK } from "@/sdk"
 
 export const Login = () => {
-  const { logIn, user, isLoggingIn } = web3AuthSDK
+  const { logIn, user, loggingIn } = useWeb3Auth()
 
   console.log({ logIn, user })
 
   return (
     <div className="login">
       <button type="submit" onClick={logIn} disabled={user}>
-        {isLoggingIn ? "Logging in..." : "Log in"}
+        {loggingIn ? "Logging in..." : "Log in"}
       </button>
     </div>
   )
