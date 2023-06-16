d2c190a8d5ac6c545c79bff391cc62aea900ff65
David Garcia <dave74@gmail.com>
2023-06-16 11:34:02 +0200

    implements checkContractAddressData in contract context

--- state/contract.context.js
+++ state/contract.context.js
@@ -1,23 +1,17 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { ethers } from "ethers";
-import { createContext, useContext, useState, useMemo, useEffect } from "react";
-import { useHuman } from "./human.context";
-import { useWeb3Auth } from "./web3auth.context";
+import { ethers } from "ethers"
+import { createContext, useContext, useState, useMemo, useEffect } from "react"
+import { useHuman } from "./human.context"
+import { useWeb3Auth } from "./web3auth.context"
+import { config, contractSDK } from "@/sdk"
 
-const CONTRACT = {
-  chainId: '0x13881',
-  address: '0x2CEDFf179BF88F7B4b1FFF9ca6d53393E956B74F',
-  abi: [
-    "function mint(address to, uint256 amount) public",
-    "function transfer(address to, uint256 amount) public returns (bool)",
-    "function decimals() public view returns (uint8)",
-  ],
-};
+const { CONTRACT } = config
+const { checkContractAddress } = contractSDK
 
 const ContractContext = createContext({
   loadingContract: false,
   contract: null,
-  balance: '0.0',
+  balance: "0.0",
   updatingPolicies: false,
   fullApprovedOwner: false,
   functionApprovedOwner: false,
@@ -29,29 +23,28 @@ const ContractContext = createContext({
   updateAddressStatus: async (status) => {},
   updateMask: async () => {},
   updateFunctionStatus: async (status) => {},
-});
+})
 
 function ContractProvider(props) {
-  
-  const { projectId, chainId } = useWeb3Auth();
-  const { address } = useHuman();
-  const [loadingContract, setLoadingContract] = useState(false);
-  const [contract, setContract] = useState(null);
+  const { projectId, chainId } = useWeb3Auth()
+  const { address } = useHuman()
+  const [loadingContract, setLoadingContract] = useState(false)
+  const [contract, setContract] = useState(null)
 
-  const [loadingBalance, setLoadingBalance] = useState(false);
-  const [balance, setBalance] = useState('---');
+  const [loadingBalance, setLoadingBalance] = useState(false)
+  const [balance, setBalance] = useState("---")
 
-  const [updatingPolicies, setUpdatingPolicies] = useState(false);
-  const [fullApprovedOwner, setFullApprovedOwner] = useState(false);
-  const [functionApprovedOwner, setFunctionApprovedOwner] = useState(false);
+  const [updatingPolicies, setUpdatingPolicies] = useState(false)
+  const [fullApprovedOwner, setFullApprovedOwner] = useState(false)
+  const [functionApprovedOwner, setFunctionApprovedOwner] = useState(false)
 
   const getBalance = async () => {
     try {
       setLoadingBalance(true)
-      const response = await fetch('/api/usecases/tokens/getTokenBalanceUC', {
-        method: 'POST',
+      const response = await fetch("/api/usecases/tokens/getTokenBalanceUC", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           chainId: CONTRACT.chainId,
@@ -73,11 +66,11 @@ function ContractProvider(props) {
       const filter = {
         address: CONTRACT.address,
         chainId: CONTRACT.chainId,
-      };
-      const response = await fetch('/api/usecases/contracts/getContractUC', {
-        method: 'POST',
+      }
+      const response = await fetch("/api/usecases/contracts/getContractUC", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ filter }),
       })
@@ -98,11 +91,11 @@ function ContractProvider(props) {
         abi: CONTRACT.abi,
         chainId: CONTRACT.chainId,
       }
-  
-      const response = await fetch('/api/usecases/contracts/updateContractUC', {
-        method: 'POST',
+
+      const response = await fetch("/api/usecases/contracts/updateContractUC", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(params),
       })
@@ -114,35 +107,20 @@ function ContractProvider(props) {
     setLoadingContract(false)
   }
 
-  const checkContractAddress = async () => {
-    try {
-      const result = await fetch('/api/usecases/policies/checkContractAddressUC', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          address: CONTRACT.address,
-          chainId: CONTRACT.chainId,
-        }),
-      })
-      const { response } = await result.json()
-      setFullApprovedOwner(response)
-    } catch (error) {
-      console.error(error)
-      setFullApprovedOwner(false)
-    }
+  const checkContractAddressData = async () => {
+    const response = await checkContractAddress(CONTRACT)
+    response ? setFullApprovedOwner(response) : setFullApprovedOwner(false)
   }
 
   const checkContractData = async () => {
     try {
-      const method = 'mint';
-      const params = [ethers.constants.AddressZero, ethers.constants.One];
+      const method = "mint"
+      const params = [ethers.constants.AddressZero, ethers.constants.One]
 
-      const result = await fetch('/api/usecases/policies/checkContractDataUC', {
-        method: 'POST',
+      const result = await fetch("/api/usecases/policies/checkContractDataUC", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           chainId: CONTRACT.chainId,
@@ -162,10 +140,10 @@ function ContractProvider(props) {
   const updateAddressStatus = async (status) => {
     try {
       setUpdatingPolicies(true)
-      await fetch('/api/usecases/policies/updateAddressStatusUC', {
-        method: 'POST',
+      await fetch("/api/usecases/policies/updateAddressStatusUC", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           projectId,
@@ -183,32 +161,32 @@ function ContractProvider(props) {
 
   const updateMask = async () => {
     try {
-      const selector = new ethers.utils.Interface(TOKEN_ABI).getSighash('mint');
-      const mask = ethers.utils.solidityPack(
-        ['uint32'],
-        ['0xffffffff']
-      );
+      const selector = new ethers.utils.Interface(TOKEN_ABI).getSighash("mint")
+      const mask = ethers.utils.solidityPack(["uint32"], ["0xffffffff"])
 
-      const { mask: checkMask } = await fetch('/api/usecases/policies/getMaskUC', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          chainId: CONTRACT.chainId,
-          address: CONTRACT.address,
-          method: 'mint',
-        }),
-      }).then((res) => res.json())
-    
+      const { mask: checkMask } = await fetch(
+        "/api/usecases/policies/getMaskUC",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            chainId: CONTRACT.chainId,
+            address: CONTRACT.address,
+            method: "mint",
+          }),
+        }
+      ).then((res) => res.json())
+
       if (checkMask === mask) {
-        return;
+        return
       }
 
-      await fetch('/api/usecases/policies/updateMaskUC', {
-        method: 'POST',
+      await fetch("/api/usecases/policies/updateMaskUC", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           projectId,
@@ -224,16 +202,16 @@ function ContractProvider(props) {
   }
 
   const updateFunctionStatus = async (status) => {
-    setUpdatingPolicies(true);
-    await updateMask();
-    const selector = new ethers.utils.Interface(CONTRACT.abi).getSighash('mint');
-    const selectorAndParams = ethers.utils.solidityPack(['bytes4'], [selector])
+    setUpdatingPolicies(true)
+    await updateMask()
+    const selector = new ethers.utils.Interface(CONTRACT.abi).getSighash("mint")
+    const selectorAndParams = ethers.utils.solidityPack(["bytes4"], [selector])
 
     try {
-      await fetch('/api/usecases/policies/updateFunctionStatusUC', {
-        method: 'POST',
+      await fetch("/api/usecases/policies/updateFunctionStatusUC", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           chainId: CONTRACT.chainId,
@@ -246,35 +224,33 @@ function ContractProvider(props) {
       console.error(error)
     }
 
-    checkContractAddress();
+    checkContractAddress()
     setUpdatingPolicies(false)
-
   }
-  
+
   useEffect(() => {
     getContract()
   }, [chainId])
 
   useEffect(() => {
-    if(!contract) return
+    if (!contract) return
     checkContractAddress()
     checkContractData()
 
     const interval = setInterval(() => {
       checkContractAddress()
       checkContractData()
-    }
-    , 5000);
-    return () => clearInterval(interval);
+    }, 5000)
+    return () => clearInterval(interval)
   }, [contract, updatingPolicies])
 
   useEffect(() => {
-    if(!address) return
+    if (!address) return
     getBalance()
     const interval = setInterval(() => {
       getBalance()
-    }, 5000);
-    return () => clearInterval(interval);
+    }, 5000)
+    return () => clearInterval(interval)
   }, [address])
 
   const memoizedData = useMemo(
@@ -289,26 +265,31 @@ function ContractProvider(props) {
       getBalance,
       getContract,
       updateContract,
-      checkContractAddress,
+      checkContractAddress: checkContractAddressData,
       checkContractData,
       updateAddressStatus,
       updateMask,
       updateFunctionStatus,
     }),
-    [loadingContract, contract, balance, updatingPolicies, fullApprovedOwner, functionApprovedOwner]
-  );
+    [
+      loadingContract,
+      contract,
+      balance,
+      updatingPolicies,
+      fullApprovedOwner,
+      functionApprovedOwner,
+    ]
+  )
 
-  return <ContractContext.Provider value={memoizedData} {...props} />;
+  return <ContractContext.Provider value={memoizedData} {...props} />
 }
 
 function useContract() {
-  const context = useContext(ContractContext);
+  const context = useContext(ContractContext)
   if (context === undefined) {
-    throw new Error(
-      `useContract must be used within a ContractProvider`
-    );
+    throw new Error(`useContract must be used within a ContractProvider`)
   }
-  return context;
+  return context
 }
 
-export { ContractProvider, useContract };
+export { ContractProvider, useContract }
