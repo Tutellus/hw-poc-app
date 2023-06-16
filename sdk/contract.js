2da0bfd373ea4f293de3ae960d0901f393f6b99f
David Garcia <dave74@gmail.com>
2023-06-16 11:33:04 +0200

    adds checkContractAddress to  contractSDK

--- sdk/contract.js
+++ sdk/contract.js
@@ -1,36 +1,10 @@
-const CONTRACT_DATA = {
-  chainId: "0x13881",
-  address: "0x2CEDFf179BF88F7B4b1FFF9ca6d53393E956B74F",
-  abi: [
-    "function mint(address to, uint256 amount) public",
-    "function transfer(address to, uint256 amount) public returns (bool)",
-    "function decimals() public view returns (uint8)",
-  ],
-}
-
-let contract = null
+import { GQLRepository } from "./repository"
 
 export const contractSDK = {
-  updateContract: async () => {
-    try {
-      const params = {
-        address: CONTRACT_DATA.address,
-        abi: CONTRACT_DATA.abi,
-        chainId: CONTRACT_DATA.chainId,
-      }
-
-      const response = await fetch("/api/usecases/contracts/updateContractUC", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(params),
-      })
-      const { contract: updatedContract } = await response.json()
-      contract = updatedContract
-    } catch (error) {
-      console.error(error)
-    }
+  checkContractAddress: async (CONTRACT) => {
+    const result = await GQLRepository.getContractAddress({
+      address: CONTRACT?.address,
+    })
+    return result
   },
-  contract,
 }
