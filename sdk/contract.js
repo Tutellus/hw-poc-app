2f9e763ad6b17aacc27b48f9beacf351298865de
David Garcia <dave74@gmail.com>
2023-06-16 12:35:27 +0200

    adds checkContractData to SDK

--- sdk/contract.js
+++ sdk/contract.js
@@ -1,10 +1,25 @@
 import { GQLRepository } from "./repository"
+import { ethers } from "ethers"
 
 export const contractSDK = {
   checkContractAddress: async (CONTRACT) => {
-    const result = await GQLRepository.getContractAddress({
+    const result = await GQLRepository.checkContractAddress({
       address: CONTRACT?.address,
     })
+
+    return result
+  },
+
+  checkContractData: async (CONTRACT) => {
+    const method = "mint"
+    const params = [ethers.constants.AddressZero, ethers.constants.One]
+
+    const result = await GQLRepository.checkContractData({
+      address: CONTRACT?.address,
+      method,
+      params,
+    })
+
     return result
   },
 }
