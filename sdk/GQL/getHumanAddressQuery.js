7f69ba0bc838a6b5a3508122f2c8cd4963fa757a
David Garcia <dave74@gmail.com>
2023-06-23 09:49:59 +0200

    update queries

--- sdk/GQL/getHumanAddressQuery.js
+++ sdk/GQL/getHumanAddressQuery.js
@@ -4,8 +4,18 @@ export const GET_HUMAN_ADDRESS_QUERY = gql`
   query GET_HUMAN_ADDRESS_QUERY($input: GetHumanAddressInput!) {
     getHumanAddress(input: $input) {
       __typename
-      ... on HumanAddress {
+      ... on HumanWithSG {
+        _id
         address
+        owner
+        chainId
+        email
+        projectId
+        status
+        stringSalt
+        userId
+        nonce
+        lastExecutionTime
       }
       ... on TypeError {
         message
