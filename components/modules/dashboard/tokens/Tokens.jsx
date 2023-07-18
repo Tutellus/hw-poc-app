28c0db061da93377bdd6462aa769c58817f03816
Dani <damuru88@gmail.com>
2023-07-18 14:12:01 +0200

    refactor contract context

--- components/modules/dashboard/tokens/Tokens.jsx
+++ components/modules/dashboard/tokens/Tokens.jsx
@@ -1,23 +1,20 @@
-import { useState } from "react"
-import { useContract } from "@/state/contract.context"
-import { useHuman } from "@/state/human.context"
-import { ethers } from "ethers"
+import { useState } from "react";
+import { useContract } from "@/state/contract.context";
+import { useHuman } from "@/state/human.context";
+import { ethers } from "ethers";
 
 export const Tokens = () => {
-  const { loadingContract, contract, balance, updateContract } = useContract()
-  const { human, processingProposal, requestProposal } = useHuman()
+  const { balance } = useContract();
+  const { human, processingProposal, requestProposal } = useHuman();
 
-  const [minting, setMinting] = useState(false)
+  const [minting, setMinting] = useState(false);
 
-  const canMint = human?.status === "CONFIRMED" && !minting && !processingProposal
+  const canMint = human?.status === "CONFIRMED" && !minting && !processingProposal;
 
   const requestMint = async () => {
-    setMinting(true)
+    setMinting(true);
     const contractInterface = new ethers.utils.Interface(contract.abi);
-    const calldata = contractInterface.encodeFunctionData("mint", [
-      human.address,
-      ethers.utils.parseEther("5"),
-    ]);
+    const calldata = contractInterface.encodeFunctionData("mint", [human.address, ethers.utils.parseEther("5")]);
     await requestProposal({
       title: "Mint 5 tokens",
       description: "We will mint 5 tokens for you",
@@ -26,12 +23,12 @@ export const Tokens = () => {
           target: contract.address,
           method: "mint(address,uint256)",
           data: calldata,
-          value: '0',
+          value: "0",
         },
       ],
-    })
-    setMinting(false)
-  }
+    });
+    setMinting(false);
+  };
 
   return (
     <div className="box">
@@ -46,31 +43,25 @@ export const Tokens = () => {
             gap: "1rem",
           }}
         >
-          {contract ? (
-            <div
+          <div
+            style={{
+              display: "flex",
+              gap: "8px",
+            }}
+          >
+            <button
               style={{
                 display: "flex",
-                gap: "8px",
+                alignItems: "center",
               }}
+              disabled={!canMint}
+              onClick={requestMint}
             >
-              <button
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                }}
-                disabled={!canMint}
-                onClick={requestMint}
-              >
-                {minting ? "Processing..." : "Mint 5 tokens"}
-              </button>
-            </div>
-          ) : (
-            <button disabled={loadingContract} onClick={updateContract}>
-              {loadingContract ? "Loading..." : "Load contract"}
+              {minting ? "Processing..." : "Mint 5 tokens"}
             </button>
-          )}
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
