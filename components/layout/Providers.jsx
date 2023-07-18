368504ab4baddaab667344fe13834d5cd6474cd0
Dani <damuru88@gmail.com>
2023-07-18 11:49:42 +0200

    remove magic context

--- components/layout/Providers.jsx
+++ components/layout/Providers.jsx
@@ -1,21 +1,13 @@
-import { ModalProvider } from '@/state/modal.context'
-import { ContractProvider } from '@/state/contract.context';
-import { Web3AuthProvider } from '@/state/web3auth.context';
-import { HumanProvider } from '@/state/human.context';
-import { MagicLinkProvider } from '@/state/magicLink.context';
+import { ContractProvider } from "@/state/contract.context";
+import { Web3AuthProvider } from "@/state/web3auth.context";
+import { HumanProvider } from "@/state/human.context";
 
 export const Providers = ({ children }) => {
   return (
-    <ModalProvider>
-      <Web3AuthProvider>
-      {/* <MagicLinkProvider> */}
-        <HumanProvider>
-          <ContractProvider>
-            {children}
-          </ContractProvider>
-        </HumanProvider>
-      {/* </MagicLinkProvider> */}
-      </Web3AuthProvider>
-    </ModalProvider>
-  )
+    <Web3AuthProvider>
+      <HumanProvider>
+        <ContractProvider>{children}</ContractProvider>
+      </HumanProvider>
+    </Web3AuthProvider>
+  );
 };
