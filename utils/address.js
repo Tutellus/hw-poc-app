f3127a21d6616ee6f05a36c102e65b9e32fa2833
David Garcia <dave74@gmail.com>
2023-07-26 13:53:02 +0200

    adds noLink attribute

--- utils/address.js
+++ utils/address.js
@@ -2,33 +2,32 @@ const compareAddresses = (a, b) => {
   return a.toLowerCase() === b.toLowerCase()
 }
 
-const explorerLink = ({
-  value,
-  type = 'address',
-}) => {
+const explorerLink = ({ value, type = "address" }) => {
   return `https://mumbai.polygonscan.com/${type}/${value}`
 }
 
-const truncateAddress = ({
-  address = '',
-  chars = 8,
-  stringify = false,
-  extend = false,
-}) => {
-  const value = address.slice(2, chars+2).toUpperCase();
-  if (stringify) return value;
-  return (
+const truncateAddress = (
+  { address = "", chars = 8, stringify = false, extend = false },
+  noLink
+) => {
+  const value = address.slice(2, chars + 2).toUpperCase()
+  if (stringify) return value
+  return noLink ? (
+    extend ? (
+      address
+    ) : (
+      value
+    )
+  ) : (
     <a
-      style={{ color: 'white' }}
+      style={{ color: "white" }}
       href={explorerLink({ value: address })}
       target="_blank"
       rel="noopener noreferrer"
-    >{extend ? address : value}</a>
+    >
+      {extend ? address : value}
+    </a>
   )
 }
 
-export {
-  compareAddresses,
-  truncateAddress,
-  explorerLink,
-}
+export { compareAddresses, truncateAddress, explorerLink }
