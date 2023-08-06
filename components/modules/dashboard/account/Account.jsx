0fc839d0457830b47b9394b3dc4c2592d62b556f
David Garcia <dave74@gmail.com>
2023-08-06 12:46:05 +0200

    add human data to account component

--- components/modules/dashboard/account/Account.jsx
+++ components/modules/dashboard/account/Account.jsx
@@ -1,15 +1,44 @@
-import { useSession, signOut } from "next-auth/react"
+import { useState } from "react"
+import { truncateAddress } from "@/utils/address"
+import { Button } from "@tutellus/tutellus-components/lib/components/atoms/button"
+import { CopyIcon } from "@/components/icons"
+import { signOut } from "next-auth/react"
+import cx from "classnames"
+import styles from "./account.module.css"
 
-export const Account = () => {
-  const { data: session } = useSession()
+export const Account = ({ session, human }) => {
+  const [extendedAddress, setExtendedAddress] = useState(false)
   const user = session?.user
 
+  const { address } = human || {}
+
+  const isDeploying = human?.status === "PENDING"
+  const isReady = human?.status === "CONFIRMED"
+  const isNotReady = !isDeploying && !isReady
+
   return (
-    <div className="box">
-      <div className="title">My account</div>
-      <div className="data">
+    <div className={styles.accountContainer}>
+      <div
+        className={cx(styles.account, {
+          [styles.pulse]: isDeploying,
+        })}
+        onMouseEnter={() => setExtendedAddress(true)}
+        onMouseLeave={() => setExtendedAddress(false)}
+      >
+        {address
+          ? truncateAddress({
+              address,
+              extend: extendedAddress,
+            })
+          : "No human connected"}
+        <span>
+          <CopyIcon />
+        </span>
+      </div>
+      <span className={styles.accountLabel}>Ver en el explorador</span>
+      <div className={styles.userEmail}>
         {user?.email || "No user"}
-        <button onClick={() => signOut()}>Logout</button>
+        <Button onClick={() => signOut()}>Logout</Button>
       </div>
     </div>
   )
