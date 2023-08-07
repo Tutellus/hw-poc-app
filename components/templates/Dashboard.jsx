71ff5c5324f58966ec453932ff9666efc76d02e1
David Garcia <dave74@gmail.com>
2023-08-07 17:46:58 +0200

    update dashBoard template component

--- components/templates/Dashboard.jsx
+++ components/templates/Dashboard.jsx
@@ -1,20 +1,74 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import { useSession } from "next-auth/react"
-import { Account } from "../modules/dashboard/account/Account"
-import { Tokens } from "../modules/dashboard/tokens/Tokens"
-import { ProposalsList } from "../modules/dashboard/proposals/ProposalsList"
-import { HumanWalletLogo } from "../icons"
+import { ethers } from "ethers"
+import { CONTRACT } from "@/config"
+
+import {
+  Account,
+  TrxTypePanel,
+  ProposalsList,
+  PendingProposalsList,
+} from "../modules/dashboard"
+import { HumanWalletLogo, ThumbIcon, MailIcon, FailedIcon } from "../icons"
 import { useHuman } from "@/state/human.context"
 import styles from "./Dashboard.module.css"
-import { PendingProposalsList } from "../modules/dashboard/proposals"
 
 export const Dashboard = () => {
   const { data: session } = useSession()
-  const { human } = useHuman()
+  const { human, processingProposal, requestProposal, getTokensBalance } =
+    useHuman()
 
   const router = useRouter()
-  console.log({ session })
+
+  const [balance, setBalance] = useState("0")
+  const [minting, setMinting] = useState(false)
+
+  const canMint =
+    human?.status === "CONFIRMED" && !minting && !processingProposal
+
+  const getTokenBalance = async () => {
+    try {
+      const response = await getTokensBalance([
+        {
+          token: CONTRACT.address,
+          type: "ERC20",
+        },
+      ])
+
+      if (response) {
+        const value = response.items.find(
+          (item) => item.token === CONTRACT.address
+        ).bigNumber
+        const innerBalance = ethers.utils.formatEther(value)
+        setBalance(innerBalance)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const requestMint = async () => {
+    setMinting(true)
+    const contractInterface = new ethers.utils.Interface(CONTRACT.abi)
+    const calldata = contractInterface.encodeFunctionData("mint", [
+      human.address,
+      ethers.utils.parseEther("5"),
+    ])
+    await requestProposal({
+      title: "Mint 5 tokens",
+      description: "We will mint 5 tokens for you",
+      calls: [
+        {
+          target: CONTRACT.address,
+          method: "mint(address,uint256)",
+          data: calldata,
+          value: "0",
+        },
+      ],
+    })
+    setMinting(false)
+  }
 
   useEffect(() => {
     if (!session) {
@@ -22,6 +76,14 @@ export const Dashboard = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session])
+
+  useEffect(() => {
+    getTokenBalance()
+    const interval = setInterval(() => {
+      getTokenBalance()
+    }, 5000)
+    return () => clearInterval(interval)
+  }, [human])
 
   return (
     <div className={styles.dashboardContainer}>
@@ -35,7 +97,21 @@ export const Dashboard = () => {
         <h2>Prueba distintas operaciones en nuestro Human Wallet</h2>
       </div>
       <div className={styles.modesContainer}>
-        <Tokens />
+        <TrxTypePanel
+          literal="Acción sin 2FA que se confirma"
+          icon={<ThumbIcon />}
+          callback={requestMint}
+        />{" "}
+        <TrxTypePanel
+          literal="Acción con 2FA que se confirma"
+          icon={<MailIcon />}
+          callback={requestMint}
+        />{" "}
+        <TrxTypePanel
+          literal="Acción sin 2FA que da error"
+          icon={<FailedIcon />}
+          callback={requestMint}
+        />
       </div>
       <div className={styles.proposalsContainer}>
         <PendingProposalsList />
