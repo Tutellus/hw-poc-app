dd26221e927bde9e0fd83e12c516caa34f8127c3
David Garcia <dave74@gmail.com>
2023-08-11 12:55:57 +0200

    adds proposalDetail data

--- components/modules/dashboard/proposals/ProposalDetail.jsx
+++ components/modules/dashboard/proposals/ProposalDetail.jsx
@@ -1,19 +1,36 @@
-import styles from "./proposals.module.css"
+import styles from "./proposalDetail.module.css"
 export const ProposalDetail = ({ proposal }) => {
   console.log({ proposal })
 
-  return (
+  const network = proposal?.chainId === "0x13881" ? "Mumbai" : "Unknown"
+
+  return proposal ? (
     <div className={styles.container}>
+      <h2 className={styles.title}>Details</h2>
       <div className={styles.block}>
         <div className={styles.keys}>
-          <div>Title</div>
-          <div>Description</div>
+          <span>Transaction Hash</span>
+          <span>Nonce</span>
+          <span>Required 2FA</span>
+          <span>Status</span>
+          <span>Network</span>
+          <span>Project ID</span>
+          <span>Sender</span>
         </div>
         <div className={styles.values}>
-          <div>{proposal?.title}</div>
-          <div>{proposal?.description}</div>
+          <span>{proposal.txHash}</span>
+          <span>{proposal.nonce}</span>
+          <span>{proposal.required2FA ? "ACTIVE" : "INACTIVE"}</span>
+          <span>{proposal.status}</span>
+          <span>{network}</span>
+          <span>{proposal.projectId}</span>
+          <span>{proposal.sender}</span>
         </div>
       </div>
+    </div>
+  ) : (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Loading Proposal data</h2>
     </div>
   )
 }
