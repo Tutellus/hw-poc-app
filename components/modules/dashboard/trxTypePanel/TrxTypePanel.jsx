48529b4d4e1c889bf2eef66a20fbf221fa8fabb5
David Garcia <dave74@gmail.com>
2023-08-21 15:55:10 +0200

    use accent buttons in trx proposals

--- components/modules/dashboard/trxTypePanel/TrxTypePanel.jsx
+++ components/modules/dashboard/trxTypePanel/TrxTypePanel.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { Button } from "@tutellus/tutellus-components/lib/components/atoms/button"
+import {
+  Button,
+  buttonColors,
+} from "@tutellus/tutellus-components/lib/components/atoms/button"
 import styles from "./trxTypePanel.module.css"
 import cx from "classnames"
 
@@ -9,7 +12,11 @@ export const TrxTypePanel = ({ literal, icon, callback, isDisabled }) => (
   >
     <div className={styles.title}>{literal}</div>
     <div className={styles.description}>{icon}</div>
-    <Button isDisabled={isDisabled} onClick={callback}>
+    <Button
+      color={buttonColors.ACCENT}
+      isDisabled={isDisabled}
+      onClick={callback}
+    >
       PROBAR
     </Button>
   </div>
