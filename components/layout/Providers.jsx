1a2a6752cd6735d9d3853e7ebe0e0689b21fc955
victormer <victormerinokosina@protonmail.com>
2023-09-07 12:01:45 +0200

    feat: unique context with web3auth, avoiding duplicates errors and removing unnecessary useEffects

--- components/layout/Providers.jsx
+++ components/layout/Providers.jsx
@@ -1,12 +1,9 @@
-import { Web3AuthProvider } from "@/state/web3auth.context";
 import { HumanProvider } from "@/state/human.context";
 
 export const Providers = ({ children }) => {
   return (
-    <Web3AuthProvider>
       <HumanProvider>
         {children}
       </HumanProvider>
-    </Web3AuthProvider>
   );
 };
