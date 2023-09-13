63bd572f582bd7375314478d54c42f89661b2b2b
David Garcia <dave74@gmail.com>
2023-09-13 12:59:26 +0200

    improve folder structure

--- services/index.js
+++ services/index.js
@@ -1,16 +1,1 @@
-import { authFetcher } from "./authFetcher"
-import { AUTHENTICATE_MUTATION } from "./GQL"
-
-export const GQLService = {
-  authenticateUser: async ({ email }) => {
-    const { authenticate } = await authFetcher({
-      query: AUTHENTICATE_MUTATION,
-      variables: {
-        input: {
-          email,
-        },
-      },
-    })
-    return authenticate
-  },
-}
+export * from "./auth"
