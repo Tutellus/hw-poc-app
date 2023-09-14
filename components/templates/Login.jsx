2881c7f20e1a14c849b2fd21564f02a28ced8acb
David Garcia <dave74@gmail.com>
2023-09-14 18:49:20 +0200

    fine tune UI

--- components/templates/Login.jsx
+++ components/templates/Login.jsx
@@ -10,7 +10,13 @@ import styles from "./dashboard.module.css"
 export const Login = () => {
   const { data: session, status } = useSession()
   const router = useRouter()
-  const [email, setEmail] = useState("dave74@gmail.com")
+  const [email, setEmail] = useState("insert email here")
+  const [emailError, showEmailError] = useState(false)
+
+  const isValidEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    return regex.test(email)
+  }
 
   useEffect(() => {
     session ? router.push("/dashboard") : null
@@ -19,8 +25,13 @@ export const Login = () => {
 
   const isLoading = status === "loading"
   const handleSignIn = () => {
-    console.log(">>>>> handleSignIn", email)
-    signIn("customJWT", { email })
+    if (!isValidEmail(email)) {
+      showEmailError(true)
+      return
+    } else {
+      showEmailError(false)
+      signIn("customJWT", { email })
+    }
   }
 
   return (
@@ -28,19 +39,40 @@ export const Login = () => {
       <div className={styles.logoContainer}>
         <HumanWalletDesktop />
       </div>
-      <input
-        type="text"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <Button
-        iconLeft={<DiscordIcon />}
-        onClick={handleSignIn}
-        disabled={isLoading}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          handleSignIn()
+        }}
       >
-        {isLoading ? "Logging in..." : "LOGIN WITH EMAIL"}
-      </Button>
+        <input
+          type="text"
+          placeholder="insert email here"
+          value={email}
+          onFocus={() => {
+            setEmail("")
+            showEmailError(false)
+          }}
+          onChange={(e) => setEmail(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault()
+              handleSignIn()
+            }
+          }}
+        />
+        {emailError && (
+          <p className={styles.error}>Please insert a valid email address</p>
+        )}
+        <Button
+          type="submit"
+          iconLeft={<DiscordIcon />}
+          onClick={handleSignIn}
+          disabled={isLoading}
+        >
+          {isLoading ? "Logging in..." : "LOGIN WITH EMAIL"}
+        </Button>
+      </form>
     </div>
   )
 }
