52eec06d25c51aab8819b6dc7975aa857e98080f
David Garcia <dave74@gmail.com>
2023-09-21 10:11:52 +0200

    adds mobileAnimation component

---
+++ components/modules/dashboard/mobileAnimation/mobileAnimation.jsx
@@ -0,0 +1,45 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+
+import styles from "./mobileAnimation.module.css"
+
+export const MobileAnimation = () => (
+  <div className={styles.containerAnimation}>
+    <img
+      className={styles.hwAnimCapa1}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c3fe3bf5e1543b7379_capa1.svg"
+    />
+    <img
+      className={styles.hwAnimCapa2}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c46b0bb223969e783e_capa2.svg"
+    />
+    <img
+      className={styles.hwAnimAsterisco1}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c3e7f4e8715fdb12fa_asterisco1.svg"
+    />
+    <img
+      className={styles.hwAnimAsterisco2}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9d86aab98bd3471054f_asterisco2.svg"
+    />
+    <img
+      className={styles.hwAnimText}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c3631b69ee4bfaa62b_text.svg"
+    />
+    <img
+      className={styles.hwAnimAsterisco3}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c3d23273a2e884069e_asterisco3.svg"
+    />
+    <img
+      className={styles.hwAnimAsterisco4}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c31bb873dd500a6132_asterisco4.svg"
+    />
+    <img
+      className={styles.hwAnimTrans2}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c4d23273a2e8840725_trans2.svg"
+    />
+    <img
+      className={styles.hwAnimTrans1}
+      src="https://uploads-ssl.webflow.com/64bfe0b7cf5ceb8900f47cfc/64c8f9c300a16648afe51e98_trans1.svg"
+    />
+  </div>
+)
