61284161c4d8a522b38844c11e447a5d1c46eb67
David Garcia <dave74@gmail.com>
2023-09-26 16:09:12 +0200

    remove state logic from selec component

--- components/modules/dashboard/selectProviderIcon/SelectProviderIcon.jsx
+++ components/modules/dashboard/selectProviderIcon/SelectProviderIcon.jsx
@@ -4,22 +4,11 @@ import cx from "classnames"
 
 import styles from "./selectProviderIcon.module.css"
 
-export const SelectProviderIcon = ({ isDisabled }) => {
-  const [storedProvider, setStoredProvider] = useState("mock")
-
-  useEffect(() => {
-    const localStorageProvider = localStorage.getItem("provider")
-    localStorageProvider === undefined
-      ? localStorage.setItem("provider", "mock")
-      : null
-    setStoredProvider("mock")
-  }, [])
-
-  const handleProvider = (provider) => {
-    localStorage.setItem("provider", provider)
-    setStoredProvider(provider)
-  }
-
+export const SelectProviderIcon = ({
+  isDisabled,
+  storedProvider,
+  handleProvider,
+}) => {
   const mockIconClass = cx(styles.selectorWithIcon, {
     [styles.mockIsSelected]: storedProvider === "mock",
     [styles.isDisabled]: isDisabled,
